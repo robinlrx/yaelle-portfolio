2c0e444a3ed18f8aaba683eea6dd1c388d45814e
robinlrx <robin@monkey-lab.io>
2023-01-09 00:17:29 +0100

    page design

---
+++ src/components/Lightbox.jsx
@@ -0,0 +1,23 @@
+
+import React, { useState } from 'react';
+import FsLightbox from 'fslightbox-react';
+
+const Lightbox = ({poster}) => {
+
+	const [toggler, setToggler] = useState(false);
+
+	return (
+		<>
+			<img src={poster} width="40%" alt=""onClick={() => setToggler(!toggler)} style={{cursor: 'pointer', marginBottom: '20px'}} />
+			<FsLightbox
+				toggler={toggler}
+				sources={[
+					poster
+				]}
+			/>
+
+		</>
+	);
+};
+
+export default Lightbox;
