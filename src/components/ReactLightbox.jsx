2251527ea235ac0c30e51dda3aa2dcb55570038c
robinlrx <robin@monkey-lab.io>
2023-02-07 18:05:15 +0100

    add zoom

--- src/components/ReactLightbox.jsx
+++ src/components/ReactLightbox.jsx
@@ -4,6 +4,7 @@ import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
 import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
 import "yet-another-react-lightbox/plugins/thumbnails.css";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
 
 const ReactLightbox = ({poster, sources, thumbnails}) => {
 
@@ -16,7 +17,7 @@ const ReactLightbox = ({poster, sources, thumbnails}) => {
 				open={open}
 				close={() => setOpen(false)}
 				slides={sources}
-				plugins={thumbnails == true ? [Thumbnails] : null}
+				plugins={thumbnails == true ? [Thumbnails, Zoom] : [Zoom]}
 			/>
 
 		</>
